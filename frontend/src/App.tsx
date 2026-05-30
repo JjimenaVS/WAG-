c73f24f95f47214080bca632798392ae96674599
JjimenaVS <venegassalazarjimena@gmail.com>
2026-05-29 21:04:11 -0600

    AAAAAAAA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/layout/Footer';
 import NavHeader from './components/layout/NavBar';
 
 const navOptions = ['Home', 'About', 'Blog', 'Help'];
@@ -6,7 +7,10 @@ function App() {
   return (
     <div>
       <NavHeader logo="WAG!" options={navOptions} />
+      <Footer logo="WAG!" options={navOptions} socialMedia={[]} />
     </div>
+
+
   );
 }
 
