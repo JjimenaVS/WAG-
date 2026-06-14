fbd13a7efd1364e506655fae66a58c0cc9163934
MaluVivas <163060986+MaluVivas@users.noreply.github.com>
2026-06-14 17:33:17 -0600

    About 100% listo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import Landing from "./components/pages/Home/Landing";
 import Signin from "./components/pages/Signin/Signin";
 import Login from "./components/pages/Login/Login";
+import AboutDisplay from "./components/pages/About/AboutDisplay";
 
 
 
 function App() {
   return (
     <div>
-      
-    <Signin></Signin>
+    
+    <Signin />
+
+    
+    {/* <Signin></Signin> */}
+    {/* <AboutDisplay />*/}
     {/*<Login></Login>*/}
     {/*<Landing></Landing>*/}
        
