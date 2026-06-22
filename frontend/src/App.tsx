f315d84a240ae470e46eff282287276aba0d041d
MaluVivas <163060986+MaluVivas@users.noreply.github.com>
2026-06-21 19:40:51 -0600

    imports actualizados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Signin from "./components/pages/Signin/Signin";
 import Login from "./components/pages/Login/Login";
 import AboutDisplay from "./components/pages/About/AboutDisplay";
 import BlogDisplay from "./components/pages/Blog/BlogDisplay";
+import SpeciesArticles from "./components/pages/Blog/SpeciesArticles";
+import ArticleDisplay from "./components/pages/Blog/ArticleDisplay";
+
 
 function App() {
   return (
@@ -14,6 +17,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<AboutDisplay />} />
         <Route path="/blog" element={<BlogDisplay />} />
+      
+      <Route path="/blog/:species" element={<SpeciesArticles />} />
+      <Route path="/article/:slug" element={<ArticleDisplay />} />
+      
       </Routes>
     </BrowserRouter>
   );
